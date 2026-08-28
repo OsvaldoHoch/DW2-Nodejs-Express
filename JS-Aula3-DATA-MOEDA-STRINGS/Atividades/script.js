41c573974c5b90f87341ded3f9453a5a7691b9ce
DSM2 <fatec-dsm2@academico.intra>
2026-08-27 21:44:54 -0300

    att

--- JS-Aula3-DATA-MOEDA-STRINGS/Atividades/script.js
+++ JS-Aula3-DATA-MOEDA-STRINGS/Atividades/script.js
@@ -1,4 +1,6 @@
 //Atividade 3
+
+//CLASSE HEROI -------------------
 class Heroi {
     //Atributos
     constructor(nome, vida, velocidade, forca) {
@@ -20,21 +22,55 @@ class Heroi {
     defender() {
         return "O Herói está se defendendo";
     }
+};
+
+
+//HOMEM ARANHA ---------------------------------------------------------------------
+const homemAranha = new Heroi("Pedro Parque", "500", "100km/h", "300");
+
+homemAranha.teia = 1;
+
+function teia() {
+    if(homemAranha.teia == 1){
+        
+    }
 }
 
-const homemAranha = new Heroi("Pedro Parque", "200", "Ágil", "300");
+homemAranha.sentidoAranha = function(){
+    document.write("<p>O Herói detectou um perigo!</p>");
+};
 
-homemAranha.teia = (function(teia){
-    if(teia == 1){
-        document.write("<h1>Vai TEIA</h1>");
+homemAranha.sentidoAranha();
+
+
+// SUPERMAN -------------------------------------------------------------------------
+const superman = new Heroi("Clarke Quente", "10000", "1200km/h", "10000");
+
+superman.podeVoar = (function(voar){
+    if(voar == 1) {
+        document.write("<p>Superman está voando</p>");
     } else {
-        document.write("<p>Vem teia... </p>");
+        document.write("<p>Superman não está voando</p>");
     }
 })(1);
 
-homemAranha.sentidoAranha = function(){
-}
+superman.visaoCalor = function(){
+    document.write("<p>O Herói está usando sua visão de calor</p>");
+};
 
-const superman = new Heroi("Clarke Quente", "10000", "Velocidade do Som", "10000");
+superman.visaoCalor();
 
-superman.podeVoar = 1;
+
+// BATMAN --------------------------------------------------------------------------
+const batman = new Heroi("Bruce Waine", "100", "20km/h", "200");
+
+batman.escondido = (function(escondido){
+    if(escondido == 1) {
+        document.write("<p>Batman está escondido</p>");
+    } else {
+        document.write("<p>Batman está amostra</p>");
+    }
+})(1);
+
+
+
